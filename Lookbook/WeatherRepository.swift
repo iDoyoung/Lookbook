b0c4d82ce536e91ffcaeee357ab0620ccf0af862
iDoyoung <ido.young@icloud.com>
2024-05-23 15:20:35 +0900

    Add Request Weather Use Case With Weather Repository

---
+++ Lookbook/WeatherRepository.swift
@@ -0,0 +1,23 @@
+import Foundation
+import WeatherKit
+import CoreLocation
+
+protocol WeatherRepositoryProtocol {
+    func requestWeather(with location: CLLocation) async throws -> Weather
+}
+
+class WeatherRepository {
+    
+    private let service = WeatherService.shared
+    var currentWeathers: [CLLocation: Weather] = [:]
+    
+    func requestWeather(with location: CLLocation) async throws -> Weather {
+        if let weather = currentWeathers[location] {
+            return weather
+        } else {
+            let weather = try await service.weather(for: location)
+            currentWeathers[location] = weather
+            return weather
+        }
+    }
+}
