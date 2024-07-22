a2d1507f28e140d3b0ebd17174e547b6e921efc6
iDoyoung <ido.young@icloud.com>
2024-07-22 23:13:59 +0900

    Add Propeties Of Today Model To Display Outfit Photos

--- Lookbook/TodayModel.swift
+++ Lookbook/TodayModel.swift
@@ -1,9 +1,29 @@
 import Foundation
+import Photos
+import UIKit
 
 @Observable
 class TodayModel {
-    var photosAuthorizationStatus: PhotosAuthStatus = .unknowned
+    var photosAuthorizationStatus: PhotosAuthStatus? = nil
     var locationAuthorizationStatus: LocationAuthorizationStatus = .unauthorized
     var location: LocationInfo? = nil
-    var weather: WeatherData? = nil
+    var weather: CurrentlyWeather? = nil
+    var photosData: [Data] = []
+    var lastYearWeathers: [DailyWeather]? = nil
+    var photosAssets: [PHAsset] = []
+    
+    var outfitPhotos: [OutfitPhoto] {
+        return photosAssets.map { asset in
+            let filteredWeather = lastYearWeathers?.filter({ weather in
+                guard let assetCreateDate = asset.creationDate else { return false }
+                return Date.isEqual(date1: weather.date, date2: assetCreateDate)
+            }).first
+            
+            return OutfitPhoto(
+                asset: asset,
+                highTemp: filteredWeather?.maximumTemperature ?? "",
+                lowTemp: filteredWeather?.minimumTemperature ?? ""
+            )
+        }
+    }
 }
