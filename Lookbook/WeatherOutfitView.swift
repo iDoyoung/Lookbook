e550bc407d13393d60f58f131fac4188e7829adf
iDoyoung <ido.young@icloud.com>
2024-07-22 23:20:53 +0900

    Add Weather Outfit View

---
+++ Lookbook/WeatherOutfitView.swift
@@ -0,0 +1,75 @@
+import SwiftUI
+import Photos
+import CoreLocation
+
+struct WeatherOutfitView: View {
+    var photoAsset: PHAsset
+    var location: CLLocation?
+    
+    @State var date: String?
+    @State var locationName: String? = nil
+    @State var lowTemperature: String
+    @State var highTemperature: String
+    @State private var imageData: Data? = nil
+    
+    var body: some View {
+        
+        ZStack {
+            if let data = imageData,
+               let uiImage = UIImage(data: data) {
+                Image(uiImage: uiImage)
+                    .resizable()
+                    .scaledToFit()
+                    .onDisappear {
+                        imageData = nil
+                    }
+            } else {
+                ProgressView()
+                    .task {
+                        self.imageData = await photoAsset.data()
+                    }
+            }
+            
+            VStack() {
+                Text(locationName ?? "")
+                    .foregroundStyle(.white)
+                    .font(
+                        .system(
+                            size: 16,
+                            weight: .light,
+                            design: .monospaced)
+                    )
+                    .padding(.vertical, 5)
+                    .task {
+                        locationName = await location?.name
+                    }
+                
+                HStack {
+                    Text(highTemperature)
+                        .foregroundStyle(.white)
+                        .font(
+                            .system(
+                                size: 20,
+                                weight: .black,
+                                design: .monospaced))
+                    
+                    Text(lowTemperature)
+                        .foregroundStyle(.white)
+                        .font(
+                            .system(
+                                size: 20,
+                                weight: .regular,
+                                design: .monospaced))
+                }
+                
+                Text(date ?? "")
+                    .foregroundStyle(.white)
+                    .font(
+                        .system(
+                            size: 16,
+                            weight: .light,
+                            design: .monospaced))
+            }
+        }
+    }
+}
