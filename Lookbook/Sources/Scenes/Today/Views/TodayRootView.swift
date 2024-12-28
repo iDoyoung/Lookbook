af41e85735d7d81da66a406fdd5a89687f554990
iDoyoung <ido.young@icloud.com>
2024-12-28 16:04:04 +0900

    Redesign UI layout and components

--- Lookbook/Sources/Scenes/Today/Views/TodayRootView.swift
+++ Lookbook/Sources/Scenes/Today/Views/TodayRootView.swift
@@ -1,47 +1,160 @@
 import SwiftUI
-import Photos
 
 struct TodayRootView: View {
     
     @State var model: TodayModel
     
     var body: some View {
-        
-        ZStack(alignment: .bottom) {
-            ScrollView {
-                LazyVStack(spacing: 0) {
-                    TodayWeatherView(model: model)
-                        .containerRelativeFrame(.vertical)
-                        .scrollTransition(
-                            axis: .vertical
-                        ) { content, phase in
-                            content
-                                .offset(y: phase.value * -300)
-                        }
-                    
-                    ForEach(model.outfitPhotos, id: \.id) { photo in
-                        WeatherOutfitView(
-                            photoAsset: photo.asset,
-                            location: (model.locationState.location, photo.location),
-                            date: photo.creationDate,
-                            lowTemperature: photo.lowTemp,
-                            highTemperature: photo.highTemp
-                        )
-                        .scrollTransition(
-                            axis: .vertical
-                        ) { content, phase in
-                            content
-                                .offset(y: phase.value > 0 ? phase.value : phase.value * -300)
-                        }
-                        .containerRelativeFrame(.vertical)
-                        .clipShape(Rectangle())
-                        .shadow(radius: 10)
-                    }
+        VStack {
+            VStack {
+                HStack(alignment: .center) {
+                    Text(Image(systemName: model.symbolName ?? "questionmark"))
+                    Text(model.weatherCondition)
+                        .font(
+                            .system(
+                                size: 16,
+                                weight: .light,
+                                design: .monospaced))
+                    Spacer()
                 }
-                .scrollTargetLayout()
+                .padding(.bottom, 4)
+                
+                HStack {
+                    Text(model.currentTemperature)
+                        .font(
+                            .system(
+                                size: 16,
+                                weight: .bold,
+                                design: .monospaced))
+                    Spacer()
+                    Text("작년 12월 31일과 비슷한 기온이에요")
+                        .font(
+                            .system(
+                                size: 14,
+                                weight: .light,
+                                design: .monospaced))
+                }
             }
-            .scrollTargetBehavior(.paging)
+            .padding()
+            .debugBorder()
+            
+            ScrollView {
+                LazyVStack {
+                    VStack(alignment: .leading) {
+                        Text("2023.12.31 STYLE")
+                            .font(.custom("Futura", size: 20))
+                            .italic()
+                            .debugBorder()
+                        
+                        DataImageView(photoAsset: model.photosState.assets?.first)
+                            .border(
+                                Color(uiColor: .label),
+                                width: 2
+                            )
+                            .debugBorder()
+                        
+                        HStack {
+                            Text(model.maximumTemperature)
+                                .font(
+                                    .system(
+                                        size: 16,
+                                        weight: .bold,
+                                        design: .monospaced))
+                            Text("⎮")
+                            Text(model.minimumTemperature)
+                                .font(
+                                    .system(
+                                        size: 16,
+                                        weight: .regular,
+                                        design: .monospaced))
+                        }
+                        .debugBorder()
+                        
+                        GoogleAdBannerView()
+                            .frame(
+                                height: 50,
+                                alignment: .bottom)
+                            .debugBorder()
+                        
+                        
+                        Divider()
+                            .padding(.vertical)
+                        
+                        HStack(alignment: .center) {
+                            Text("2023년 12월의 옷차림들")
+                                .font(
+                                    .system(
+                                        size: 16,
+                                        weight: .bold,
+                                        design: .monospaced))
+                            
+                            Spacer()
+                            Text("기온차")
+                                .font(
+                                    .system(
+                                        size: 16,
+                                        weight: .bold,
+                                        design: .monospaced))
+                                .padding(.vertical, 6)
+                                .padding(.horizontal)
+                                .background(.mint)
+                                .clipShape(RoundedRectangle(cornerRadius: 20))
+                        }
+                        .padding(.top, 6)
+                        .debugBorder()
+                        
+                        //MARK: - Weather Kit
+                        VStack {
+                            HStack {
+                                Image(systemName: "apple.logo")
+                                    .font(
+                                        .system(
+                                            size: 30))
+                                Text("Weather")
+                                    .font(
+                                        .system(
+                                            size: 40,
+                                            weight: .medium))
+                            }
+                            .padding()
+                            
+                            Text("Other Apple Weather data Sources")
+                                .foregroundStyle(.blue)
+                                .onTapGesture {
+                                    if let url = URL(string: "https://developer.apple.com/weatherkit/data-source-attribution/") {
+                                        UIApplication.shared.open(url)
+                                    }
+                                }
+                                .padding(.bottom)
+                        }
+                        .frame(maxWidth: .infinity)
+                        .padding()
+                    }
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding()
+                    .debugBorder()
+                }
+                .debugBorder()
+            }
+            .frame(maxWidth: .infinity)
+            .debugBorder()
         }
-        .ignoresSafeArea()
+        .frame(maxWidth: .infinity)
     }
+    
+    var currentWeather: some View {
+        HStack {
+            
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    TodayRootView(
+        model: .init(
+            locationState: .init(),
+            photosState: .init()
+        )
+    )
 }
