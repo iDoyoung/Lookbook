b597c2d7d40de0cca6a779f1f3c3beb0104c35fc
iDoyoung <ido.young@icloud.com>
2025-01-14 17:29:31 +0900

    Add Pinch Gesture To Zoom Image

--- Lookbook/Sources/Scenes/Details/DetailsRootView.swift
+++ Lookbook/Sources/Scenes/Details/DetailsRootView.swift
@@ -3,50 +3,72 @@ import SwiftUI
 struct DetailsRootView: View {
     var model: DetailsModel
     
+    @State private var imageScale = 1.0
+    @GestureState private var magnifyBy = 1.0
+   
     var body: some View {
-        VStack {
-            if let photoData = model.imageData,
-               let uiImage = UIImage(data: photoData){
-                Rectangle()
-                    .fill(.clear)
-                    .aspectRatio(3/4, contentMode: .fit)
-                    .background(
-                        Image(uiImage: uiImage)
-                            .resizable()
-                            .scaledToFill()
-                    )
-                    .clipped()
-            } else {
-                Rectangle()
-            }
-            
-            VStack(alignment: .leading) {
-                // 위치
-                HStack {
-                    Text("테스트")
+        GeometryReader { geometry in
+            ZStack(alignment: .bottom) {
+                if let photoData = model.imageData,
+                   let uiImage = UIImage(data: photoData){
+                    Image(uiImage: uiImage)
+                        .resizable()
+                        .scaledToFill()
+                        .scaleEffect(magnifyBy*imageScale)
+                        .frame(
+                            width: geometry.size.width
+                        )
+                        .clipped()
+                        .gesture(magnification)
+                } else {
+                    Rectangle()
                 }
-                // 날씨
-                Text("\(Date().longStyle)")
-                    .dateFont()
-                    .padding(.bottom)
                 
-                // 최고 최저 기온
-                HStack {
-                    Text("12°")
-                        .temperatureFont(weight: .bold)
-                    Text("")
-                    Text("2°")
-                        .temperatureFont()
+                VStack(alignment: .leading) {
+                    // 위치
+                    HStack {
+                        Text(model.location ?? "위치 없음")
+                    }
+                    // 날씨
+                    Text("\(Date().longStyleWithTime)")
+                        .dateFont()
+                        .padding(.bottom)
+                    
+                    // 최고 최저 기온
+                    HStack {
+                        Text("12°")
+                            .temperatureFont(weight: .bold)
+                        Text("")
+                        Text("2°")
+                            .temperatureFont()
+                    }
+                    .padding(.bottom)
                 }
-                .padding(.bottom)
+                .frame(
+                    maxWidth: .infinity,
+                    alignment: .leading
+                )
+                .padding()
+                .background(.regularMaterial)
             }
-            .frame(
-                maxWidth: .infinity,
-                alignment: .leading
-            )
-            .padding()
-            .debugBorder(Color.red)
+            .ignoresSafeArea()
         }
+    }
+    
+    var magnification: some Gesture {
+        MagnifyGesture()
+            .updating($magnifyBy) { value, gestureState, transaction in
+                gestureState = value.magnification
+            }
+            .onEnded { value in
+                imageScale *= value.magnification
+                if imageScale < 1 {
+                    //FIXME: - 빠르게 할 경우 애니메이션 꼬임, 깜빡임
+                    withAnimation(.spring) {
+                        imageScale = 1
+                    }
+                }
+            }
     }
 }
 
