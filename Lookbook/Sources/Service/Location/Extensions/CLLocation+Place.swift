78c42e3ba6a7b7c05583b67d2ba0813cd78565ed
iDoyoung <ido.young@icloud.com>
2025-01-22 19:04:24 +0900

    Add Core Location Extenstion About Place

---
+++ Lookbook/Sources/Service/Location/Extensions/CLLocation+Place.swift
@@ -0,0 +1,15 @@
+import CoreLocation
+
+extension CLLocation {
+    static var seoul: CLLocation {
+        CLLocation(latitude: 37.566, longitude: 126.978)
+    }
+    
+    static var newYork: CLLocation {
+        CLLocation(latitude: 50.710, longitude: -74.006)
+    }
+    
+    static var tokyo: CLLocation {
+        CLLocation(latitude: 35.6895, longitude: 139.6917)
+    }
+}
